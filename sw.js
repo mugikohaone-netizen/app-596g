53eca2b1e9200e6d7e1a07664d265cf6f61bd85a
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-06-21 04:14:38 +0900

    🔄 SW更新 vmqmqleez

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mqman4my';
+const CACHE = 'app-mqmqleez';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
