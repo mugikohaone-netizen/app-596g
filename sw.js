2240c79cecf6be605f88195b4013ccea68699440
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-06-20 20:48:05 +0900

    🔄 SW更新 vmqman4my

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mqm9h6gt';
+const CACHE = 'app-mqman4my';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
